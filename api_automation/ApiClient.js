c3b90db5e206e50ed56c78c698a6cc6e13666bc3
Nikhil Joshi <nikhilsubhashjoshi1993@gmail.com>
2025-09-03 11:28:22 +0530

    added code for api

--- api_automation/ApiClient.js
+++ api_automation/ApiClient.js
@@ -10,17 +10,47 @@ class ApiClient {
         const options = {
             method: method,
             headers: {
-                'Content-Type': 'application/json'
+                'accept': 'application/json'
             }
         };
+
+        if (body) {
+            const isMultiPart = typeof body === 'object' && Object.values(body).some(
+                value => typeof value === 'object' && typeof value.pipe === 'function');
+            if (isMultiPart) {
+                options.multipart = body;
+            } else {
+                options.headers['Content-Type'] = 'application/json';
+                options.data = JSON.stringify(body);
+            }
+
+        }
 
         if (['POST', 'PUT', 'PATCH'].includes(method.toUpperCase()) && requestBody) {
             options.data = requestBody;
         }
-
+        console.info(`Sending ${method} request to ${this.baseURL}${endpoint}`);
         const response = await reqContext.fetch(`${this.baseURL}${endpoint}`, options);
-        await reqContext.dispose();
-        return response;
+        
+        if(response.status() >= 400) {
+            console.error(`Request failed with status ${response.status()}`);
+        }
+        try {
+            const contentType = response.headers()['content-type'];
+            let responseBody;
+            if (contentType && contentType.includes('application/json')) {
+                const responseBody = await response.json();
+                return { status: response.status(), body: responseBody };
+            } else {
+                const responseBody = await response.text();
+                return { status: response.status(), body: responseBody };
+            }
+            return response;
+        }
+        catch (error) {
+            console.error('Error parsing response:', error);
+            throw response;
+        }
     }
 }
 
